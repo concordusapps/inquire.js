864579dbb943fe71acb9421c642ffb4da9524e11
joneshf <jones3.hardy@gmail.com>
2014-03-05 13:05:18 -0800

    Lots of combinators.

--- src/data.purs
+++ src/data.purs
@@ -1,10 +1,21 @@
 module Data.Foldable where
 
+  import Prelude
+  import Data.Monoid
+
   class Foldable f where
     foldr :: forall a b. (a -> b -> b) -> b -> f a -> b
+    foldl :: forall a b. (b -> a -> b) -> b -> f a -> b
 
   class BiFoldable f where
     bifoldr :: forall a b c. (a -> c -> c) -> (b -> c -> c) -> c -> f a b -> c
+    bifoldl :: forall a b c. (c -> a -> c) -> (c -> b -> c) -> c -> f a b -> c
+
+  foldMap :: forall a f m. (Foldable f, Monoid m) => (a -> m) -> f a -> m
+  foldMap f t = foldr ((<>) <<< f) mempty t
+
+  -- filter :: forall a f t. (Applicative f, Foldable t, Monoid (f a)) => (a -> Boolean) -> t a -> f a
+  -- filter p f = foldMap (\x -> if p x then pure x else mempty) f
 
 module Data.Functor where
 
--- src/inquire.purs
+++ src/inquire.purs
@@ -1,9 +1,10 @@
 module Inquire where
 
   import Prelude
-  import Algebra
+  import Algebra ((|&|), (|||), BooleanAlgebra, (|~|), ComplementedLattice)
   import Data.Foldable
   import Data.Functor
+  import Data.Monoid
 
   data Inquire k v = EmptyAnd
                    | EmptyOr
@@ -24,7 +25,7 @@ module Inquire where
   data WrapOp = NOBOOL
               | NOT
 
-  instance Prelude.Eq Rel where
+  instance eqRel :: Prelude.Eq Rel where
     (==) EQ EQ = true
     (==) NE NE = true
     (==) GT GT = true
@@ -35,21 +36,21 @@ module Inquire where
 
     (/=) r  r' = not (r == r')
 
-  instance Prelude.Eq JuncOp where
+  instance eqJuncOp :: Prelude.Eq JuncOp where
     (==) AND AND = true
     (==) OR  OR  = true
     (==) _   _   = false
 
     (/=) r   r'  = not (r == r')
 
-  instance Prelude.Eq WrapOp where
+  instance eqWrapOp :: Prelude.Eq WrapOp where
     (==) NOBOOL NOBOOL = true
     (==) NOT    NOT    = true
     (==) _      _      = false
 
     (/=) r      r'     = not (r == r')
 
-  instance (Eq k, Eq v) => Prelude.Eq (Inquire k v) where
+  instance eqInquire :: (Prelude.Eq k, Prelude.Eq v) => Prelude.Eq (Inquire k v) where
     (==) EmptyAnd     EmptyAnd        = true
     (==) EmptyOr      EmptyOr         = true
     (==) (Pred k r v) (Pred k' r' v') = k == k' && r == r' && v == v'
@@ -59,7 +60,7 @@ module Inquire where
 
     (/=) i  i' = not (i == i')
 
-  instance Prelude.Show Rel where
+  instance showRel :: Prelude.Show Rel where
     show EQ = "="
     show NE = "!="
     show GT = ">"
@@ -67,55 +68,65 @@ module Inquire where
     show LT = "<"
     show LE = "<="
 
-  instance Prelude.Show JuncOp where
+  instance showJuncOp :: Prelude.Show JuncOp where
     show AND = "&"
     show OR  = ";"
 
-  instance Prelude.Show WrapOp where
+  instance showWrapOp :: Prelude.Show WrapOp where
     show NOBOOL = ""
     show NOT    = "!"
 
-  instance (Show k, Show v) => Prelude.Show (Inquire k v) where
+  instance showInquire :: (Prelude.Show k, Prelude.Show v) => Prelude.Show (Inquire k v) where
     show EmptyAnd = ""
     show EmptyOr = ""
     show (Pred k r v) = show k ++ show r ++ show v
     show (Junc p o q) = "(" ++ show p ++ ")" ++ show o ++ "(" ++ show q ++ ")"
     show (Wrap o i)   = show o ++ "(" ++ show i ++ ")"
 
-  instance Prelude.Functor (Inquire k) where
+  instance functorInquire :: Prelude.Functor (Inquire k) where
     (<$>) _ EmptyAnd        = EmptyAnd
     (<$>) _ EmptyOr         = EmptyOr
     (<$>) f (Pred k r v)    = Pred k r (f v)
     (<$>) f (Junc i1 op i2) = Junc (f <$> i1) op (f <$> i2)
     (<$>) f (Wrap op i)     = Wrap op (f <$> i)
 
-  instance Data.Functor.BiFunctor Inquire where
+  instance monoidInquire :: Data.Monoid.Monoid (Inquire k v) where
+    mempty = EmptyAnd
+    (<>) i EmptyAnd = i
+    (<>) EmptyAnd i = i
+    (<>) i1 i2 = Junc i1 AND i2
+
+  instance biFunctorInquire :: Data.Functor.BiFunctor Inquire where
     (<$$>) _ _ EmptyAnd        = EmptyAnd
     (<$$>) _ _ EmptyOr         = EmptyOr
     (<$$>) f g (Pred k r v)    = Pred (f k) r (g v)
     (<$$>) f g (Junc i1 op i2) = Junc ((<$$>) f g i1) op ((<$$>) f g i2)
     (<$$>) f g (Wrap op i)     = Wrap op ((<$$>) f g i)
 
-  instance Data.Foldable.Foldable (Inquire k) where
+  instance foldableInquire :: Data.Foldable.Foldable (Inquire k) where
     foldr _ z EmptyAnd     = z
     foldr _ z EmptyOr      = z
     foldr f z (Pred _ _ v) = v `f` z
     foldr f z (Junc l _ r) = foldr f (foldr f z r) l
     foldr f z (Wrap _ i)   = foldr f z i
 
-  instance Data.Foldable.BiFoldable Inquire where
+    foldl f z i = foldr (flip f) z i
+
+  instance biFoldableInquire :: Data.Foldable.BiFoldable Inquire where
     bifoldr _ _ z EmptyAnd     = z
     bifoldr _ _ z EmptyOr      = z
     bifoldr f g z (Pred k _ v) = k `f` (v `g` z)
     bifoldr f g z (Junc l _ r) = bifoldr f g (bifoldr f g z r) l
     bifoldr f g z (Wrap _ i)   = bifoldr f g z i
 
-  instance Algebra.ComplementedLattice (Inquire k v) where
+    bifoldl f g z i = bifoldr (flip f) (flip g) z i
+
+  instance complementedLatticeInquire :: Algebra.ComplementedLattice (Inquire k v) where
     (|~|) EmptyAnd = EmptyOr
     (|~|) EmptyOr  = EmptyAnd
     (|~|) p        = Wrap NOT p
 
-  instance Algebra.BooleanAlgebra (Inquire k v) where
+  instance booleanAlgebraInquire :: Algebra.BooleanAlgebra (Inquire k v) where
     (|||) EmptyAnd p        = EmptyAnd
     (|||) p        EmptyAnd = EmptyAnd
     (|||) p        EmptyOr  = p
@@ -238,11 +249,14 @@ module Inquire where
   idempotent (Junc p AND p') | p == p' = p
   idempotent (Junc p OR  p') | p == p' = p
 
-module Inquire.Utils where
+module Inquire.Combinators where
 
+  import Prelude
   import Inquire
   import Data.Array ((:), zipWith)
-  import Data.Foldable
+  import Data.Foldable (foldr, bifoldr)
+  import Data.Maybe
+  import Data.Monoid
   import Data.Tuple
 
   -- Utilities for working with Inquire.
@@ -269,3 +283,54 @@ module Inquire.Utils where
   -- fromArray :: forall k v. [[k, v]] -> Inquire k v
   fromArray []         = EmptyAnd
   fromArray ([x,y]:zs) = (fromArray zs) `and` (x `eq` y)
+
+  -- This should be implemented with a foldr or some such,
+  -- but the kind wont work out.
+  filterVals :: forall k v. (v -> Boolean) -> Inquire k v -> Inquire k v
+  filterVals p (Pred k r v) = if p v then Pred k r v else mempty
+  filterVals p (Junc l o r) = Junc (filterVals p l) o (filterVals p r)
+  filterVals p (Wrap o i)   = Wrap o (filterVals p i)
+  filterVals p i            = i
+
+  -- This is a right based find.
+  findI :: forall k v. (Eq v) => v -> Inquire k v -> Maybe (Inquire k v)
+  findI v (Pred k r v') | v == v' = Just (Pred k r v)
+  findI v (Pred k r v')           = Nothing
+  findI v (Junc l _ r)            = maybe (findI v l) Just (findI v r)
+  findI v (Wrap _ i)              = findI v i
+  findI _ _                       = Nothing
+
+  remove' :: forall k v. (Eq k, Eq v) => (Inquire k v -> Inquire k v -> Boolean) -> Inquire k v -> Inquire k v -> Inquire k v
+  remove' p i (Junc l o r) =
+    let r' = remove' p i r in
+    if p r r' then Junc (remove' p i l) o r else Junc l o r'
+  remove' p i (Wrap o i')  = Wrap o (remove' p i i')
+  remove' p i i' | i == i' = EmptyAnd
+  remove' p i i'           = i'
+
+  removeI :: forall k v. (Eq k, Eq v) => Inquire k v -> Inquire k v -> Inquire k v
+  removeI = remove' (==)
+
+  removeAllI :: forall k v. (Eq k, Eq v) => Inquire k v -> Inquire k v -> Inquire k v
+  removeAllI = remove' (\x y -> true)
+
+  foreign import find "function find(v) {\
+                      \  return function(i) {\
+                      \    /* We use String's eq typeclass because it uses `unsafeRefEq`*/\
+                      \    return findI(_ps.Prelude.eqString({}))(v)(i);\
+                      \  }\
+                      \}" :: forall k v. v -> Inquire k v -> Maybe (Inquire k v)
+
+  foreign import remove "function remove(i1) {\
+                        \  return function(i2) {\
+                        \    /* We use String's eq typeclass because it uses `unsafeRefEq`*/\
+                        \    return removeI(_ps.Prelude.eqString({}))(_ps.Prelude.eqString({}))(i1)(i2);\
+                        \  }\
+                        \}" :: forall k v. v -> Inquire k v -> Inquire k v -> Inquire k v
+
+  foreign import removeAll "function removeAll(i1) {\
+                           \  return function(i2) {\
+                           \    /* We use String's eq typeclass because it uses `unsafeRefEq`*/\
+                           \    return removeAllI(_ps.Prelude.eqString({}))(_ps.Prelude.eqString({}))(i1)(i2);\
+                           \  }\
+                           \}" :: forall k v. v -> Inquire k v -> Inquire k v -> Inquire k v
