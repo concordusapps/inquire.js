686814c93629ba96e01d65c36ed9c6481a0fe69f
joneshf <jones3.hardy@gmail.com>
2014-03-05 09:52:30 -0800

    Renamed Op's to uppercase. Added some utilities.

--- src/inquire.purs
+++ src/inquire.purs
@@ -2,8 +2,10 @@ module Inquire where
 
   import Prelude
   import Algebra
+  import Data.Array ((:), zipWith)
   import Data.Foldable
   import Data.Functor
+  import Data.Tuple
 
   data Inquire k v = EmptyAnd
                    | EmptyOr
@@ -18,11 +20,11 @@ module Inquire where
            | LT
            | LE
 
-  data JuncOp = And
-              | Or
+  data JuncOp = AND
+              | OR
 
-  data WrapOp = NoBool
-              | Not
+  data WrapOp = NOBOOL
+              | NOT
 
   instance Prelude.Eq Rel where
     (==) EQ EQ = true
@@ -36,15 +38,15 @@ module Inquire where
     (/=) r  r' = not (r == r')
 
   instance Prelude.Eq JuncOp where
-    (==) And And = true
-    (==) Or  Or  = true
+    (==) AND AND = true
+    (==) OR  OR  = true
     (==) _   _   = false
 
     (/=) r   r'  = not (r == r')
 
   instance Prelude.Eq WrapOp where
-    (==) NoBool NoBool = true
-    (==) Not    Not    = true
+    (==) NOBOOL NOBOOL = true
+    (==) NOT    NOT    = true
     (==) _      _      = false
 
     (/=) r      r'     = not (r == r')
@@ -68,12 +70,12 @@ module Inquire where
     show LE = "<="
 
   instance Prelude.Show JuncOp where
-    show And = "&"
-    show Or  = ";"
+    show AND = "&"
+    show OR  = ";"
 
   instance Prelude.Show WrapOp where
-    show NoBool = ""
-    show Not    = "!"
+    show NOBOOL = ""
+    show NOT    = "!"
 
   instance (Show k, Show v) => Prelude.Show (Inquire k v) where
     show EmptyAnd = ""
@@ -113,20 +115,20 @@ module Inquire where
   instance Algebra.ComplementedLattice (Inquire k v) where
     (|~|) EmptyAnd = EmptyOr
     (|~|) EmptyOr  = EmptyAnd
-    (|~|) p        = Wrap Not p
+    (|~|) p        = Wrap NOT p
 
   instance Algebra.BooleanAlgebra (Inquire k v) where
     (|||) EmptyAnd p        = EmptyAnd
     (|||) p        EmptyAnd = EmptyAnd
     (|||) p        EmptyOr  = p
     (|||) EmptyOr  p        = p
-    (|||) p        q        = Junc p Or q
+    (|||) p        q        = Junc p OR q
 
     (|&|) EmptyOr  p        = EmptyOr
     (|&|) p        EmptyOr  = EmptyOr
     (|&|) p        EmptyAnd = p
     (|&|) EmptyAnd p        = p
-    (|&|) p        q        = Junc p And q
+    (|&|) p        q        = Junc p AND q
 
   -- FIXME
   -- There should be some better way to `generate` from js.
@@ -140,24 +142,52 @@ module Inquire where
                           \  return gen(showDict)(showDict)(i);\
                           \}" :: forall k v. Inquire k v -> String
 
+  -- FIXME
+  -- Purescript is depending on an extends on the object.
+  foreign import objExtend "function objExtend(oldO) {\
+                           \  return function(newO) {\
+                           \    var obj = {};\
+                           \    for (var k in oldO) {\
+                           \      obj[k] = oldO[k];\
+                           \    }\
+                           \    for (var k in newO) {\
+                           \      obj[k] = newO[k];\
+                           \    }\
+                           \    return obj;\
+                           \  }\
+                           \}" :: forall r r' r''. { | r} -> { | r'} -> { | r''}
+
   gen :: forall k v. (Show k, Show v) => Inquire k v -> String
   gen i = show i
 
   pred :: forall k v. {key :: k, val :: v, rel :: Rel} -> Inquire k v
   pred o = Pred o.key o.rel o.val
 
-  eq :: forall k v. {key :: k, val :: v} -> Inquire k v
-  eq o = pred {key: o.key, rel: EQ, val: o.val}
-  ne :: forall k v. {key :: k, val :: v} -> Inquire k v
-  ne o = pred {key: o.key, rel: NE, val: o.val}
-  gt :: forall k v. {key :: k, val :: v} -> Inquire k v
-  gt o = pred {key: o.key, rel: GT, val: o.val}
-  ge :: forall k v. {key :: k, val :: v} -> Inquire k v
-  ge o = pred {key: o.key, rel: GE, val: o.val}
-  lt :: forall k v. {key :: k, val :: v} -> Inquire k v
-  lt o = pred {key: o.key, rel: LT, val: o.val}
-  le :: forall k v. {key :: k, val :: v} -> Inquire k v
-  le o = pred {key: o.key, rel: LE, val: o.val}
+  eq :: forall k v. k -> v -> Inquire k v
+  eq k v = eqObj {key: k, val: v}
+  ne :: forall k v. k -> v -> Inquire k v
+  ne k v = neObj {key: k, val: v}
+  gt :: forall k v. k -> v -> Inquire k v
+  gt k v = gtObj {key: k, val: v}
+  ge :: forall k v. k -> v -> Inquire k v
+  ge k v = geObj {key: k, val: v}
+  lt :: forall k v. k -> v -> Inquire k v
+  lt k v = ltObj {key: k, val: v}
+  le :: forall k v. k -> v -> Inquire k v
+  le k v = leObj {key: k, val: v}
+
+  eqObj :: forall k v. {key :: k, val :: v} -> Inquire k v
+  eqObj o = pred {key: o.key, rel: EQ, val: o.val}
+  neObj :: forall k v. {key :: k, val :: v} -> Inquire k v
+  neObj o = pred {key: o.key, rel: NE, val: o.val}
+  gtObj :: forall k v. {key :: k, val :: v} -> Inquire k v
+  gtObj o = pred {key: o.key, rel: GT, val: o.val}
+  geObj :: forall k v. {key :: k, val :: v} -> Inquire k v
+  geObj o = pred {key: o.key, rel: GE, val: o.val}
+  ltObj :: forall k v. {key :: k, val :: v} -> Inquire k v
+  ltObj o = pred {key: o.key, rel: LT, val: o.val}
+  leObj :: forall k v. {key :: k, val :: v} -> Inquire k v
+  leObj o = pred {key: o.key, rel: LE, val: o.val}
 
   and :: forall k v. Inquire k v -> Inquire k v -> Inquire k v
   and i1 i2 = i1 |&| i2
@@ -174,38 +204,63 @@ module Inquire where
   equiv :: forall k v. Inquire k v -> Inquire k v -> Inquire k v
   equiv p q = (p |&| q) ||| ((|~|) p |&| (|~|) q)
 
+  -- Utilities for working with Inquire.
+  toObj :: forall k v. Inquire k v -> {keys :: [k], vals :: [v]}
+  toObj i =
+    let updateVals = (\v o -> objExtend o {vals: v : o.vals}) in
+    let updateKeys = (\k o -> objExtend o {keys: k : o.keys}) in
+    bifoldr updateKeys updateVals {keys: [], vals: []} i
+
+  keys :: forall k v. Inquire k v -> [k]
+  keys i = (toObj i).keys
+
+  vals :: forall k v. Inquire k v -> [v]
+  vals i = (toObj i).vals
+
+  toTuple :: forall k v. Inquire k v -> [Tuple k v]
+  toTuple i = zip (keys i) (vals i)
+
+  -- The ideal type would be
+  -- toArray :: forall k v. Inquire k v -> [[k, v]]
+  toArray i = zipWith (\x y -> [x,y]) (keys i) (vals i)
+
+  -- The ideal type would be
+  -- fromArray :: forall k v. [[k, v]] -> Inquire k v
+  fromArray [] = EmptyAnd
+  fromArray ([x,y]:zs) = (fromArray zs) `and` (x `eq` y)
+
   -- These should all be part of BooleanAlgebra, but no bueno at this momento.
 
   absorb :: forall k v. (Eq k, Eq v) => Inquire k v -> Inquire k v
-  absorb (Junc p And (Junc p' Or  _)) | p == p' = p
-  absorb (Junc p Or  (Junc p' And _)) | p == p' = p
+  absorb (Junc p AND (Junc p' OR  _)) | p == p' = p
+  absorb (Junc p OR  (Junc p' AND _)) | p == p' = p
 
   associate :: forall k v. Inquire k v -> Inquire k v
-  associate (Junc p And (Junc q And r)) = (p |&| q) |&| r
-  associate (Junc p Or  (Junc q Or  r)) = (p ||| q) ||| r
-  associate (Junc (Junc p And q) And r) = p |&| (q |&| r)
-  associate (Junc (Junc p Or  q) Or  r) = p ||| (q ||| r)
+  associate (Junc p AND (Junc q AND r)) = (p |&| q) |&| r
+  associate (Junc p OR  (Junc q OR  r)) = (p ||| q) ||| r
+  associate (Junc (Junc p AND q) AND r) = p |&| (q |&| r)
+  associate (Junc (Junc p OR  q) OR  r) = p ||| (q ||| r)
 
   assocLeft :: forall k v. Inquire k v-> Inquire k v
-  assocLeft (Junc p And (Junc q And r)) = (p |&| q) |&| r
-  assocLeft (Junc p Or  (Junc q Or  r)) = (p ||| q) ||| r
+  assocLeft (Junc p AND (Junc q AND r)) = (p |&| q) |&| r
+  assocLeft (Junc p OR  (Junc q OR  r)) = (p ||| q) ||| r
 
   assocRight :: forall k v. Inquire k v-> Inquire k v
-  assocRight (Junc (Junc p And q) And r) = p |&| (q |&| r)
-  assocRight (Junc (Junc p Or  q) Or  r) = p ||| (q ||| r)
+  assocRight (Junc (Junc p AND q) AND r) = p |&| (q |&| r)
+  assocRight (Junc (Junc p OR  q) OR  r) = p ||| (q ||| r)
 
   commute :: forall k v. Inquire k v -> Inquire k v
-  commute (Junc p And q) = q |&| p
-  commute (Junc p Or  q) = q ||| p
+  commute (Junc p AND q) = q |&| p
+  commute (Junc p OR  q) = q ||| p
 
   distribute :: forall k v. Inquire k v -> Inquire k v
-  distribute (Junc p And (Junc q Or  r)) = (p ||| q) |&| (p ||| r)
-  distribute (Junc p Or  (Junc q And r)) = (p |&| q) ||| (p |&| r)
+  distribute (Junc p AND (Junc q OR  r)) = (p ||| q) |&| (p ||| r)
+  distribute (Junc p OR  (Junc q AND r)) = (p |&| q) ||| (p |&| r)
 
   codistribute :: forall k v. Inquire k v -> Inquire k v
-  codistribute (Junc (Junc p Or  q) And (Junc p Or  r)) = p |&| (q ||| r)
-  codistribute (Junc (Junc p And q) Or  (Junc p And r)) = p ||| (q |&| r)
+  codistribute (Junc (Junc p OR  q) AND (Junc p OR  r)) = p |&| (q ||| r)
+  codistribute (Junc (Junc p AND q) OR  (Junc p AND r)) = p ||| (q |&| r)
 
   idempotent :: forall k v. (Eq k, Eq v) => Inquire k v -> Inquire k v
-  idempotent (Junc p And p') | p == p' = p
-  idempotent (Junc p Or  p') | p == p' = p
+  idempotent (Junc p AND p') | p == p' = p
+  idempotent (Junc p OR  p') | p == p' = p
